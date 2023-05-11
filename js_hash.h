e7f383751e6786220683fe3856ce5a3cb15b6ecb
RUANG <108215543+rruuaanng@users.noreply.github.com>
2023-05-11 14:18:53 +0800

    Update js_hash.h

--- js_hash.h
+++ js_hash.h
@@ -2,7 +2,7 @@
 #define __JS_HASH_H
 
 __UINT32_TYPE__
-JSHash(const char *str){
+js_hash(const char *str){
     int hash = 1315423911;
     while(*str){
         hash ^= ((hash<<5) + (*str++) + (hash>>2));
